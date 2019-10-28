0c7601bc38f58e1582f1274781050fb96a75121d
atmaSveRo <romandronnikov@gmai.com>
2019-10-28 22:00:17 +0300

    fix minIndex function

--- array/indexMin.js
+++ array/indexMin.js
@@ -1,4 +1,7 @@
 export const indexMin = (array) => {
+    if (array.length === 0) {
+        return null
+    }
     let minElem = array[0]
     let minIndex = 0
     for(let i = 0; i < array.length; i++) {
--- array/indexMin.test.js
+++ array/indexMin.test.js
@@ -5,7 +5,7 @@ export const testIndexMin  = () => {
     const testResults = []
     expect(testResults, indexMin([11, 5, 89]), 1)
     expect(testResults, indexMin([112, 577, 890]), 0)
-    expect(testResults, indexMin([]), 0)
+    expect(testResults, indexMin([]), null)
 
     return {
         name: 'testIndexMin',

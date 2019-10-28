206b1067a88ef50a74121463419a5f24a8530d9a
atmaSveRo <romandronnikov@gmai.com>
2019-10-28 22:22:38 +0300

    fix max function

--- array/max.test.js
+++ array/max.test.js
@@ -5,7 +5,7 @@ export const testMax = () => {
     const testResults = []
     expect(testResults, max([2, 6, 1]), 6)
     expect(testResults, max([45, 78, 111]), 111)
-    expect(testResults, max([32, 11, 4]), 32)
+    expect(testResults, max([]), null)
 
     return {
         name: 'testMax',

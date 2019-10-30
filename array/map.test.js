eaf4e9d130ed662508e384aae354244c2d5a7360
atmaSveRo <romandronnikov@gmai.com>
2019-10-31 00:06:40 +0300

    fix max, map and minIndex functions

--- array/map.test.js
+++ array/map.test.js
@@ -4,7 +4,9 @@ import  { map } from './map.js'
 export const testMap = () => {
     const testResults = []
     expect(testResults, map([4, 9, 16], Math.sqrt), [2, 3, 4])
-    expect(testResults, map(5, toString()), '5')
+    expect(testResults, map([1, 2, 3, 4], (elem) => {
+        return elem.toString()
+    }), ['1', '2', '3', '4'])
     expect(testResults, map([11.576, 82.123, 8741.6546354], Math.floor), [11, 82, 8741])
 
     return {

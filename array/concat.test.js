ac18498989f0a5f7ee0c29c709129ab9c80e8dba
atmaSveRo <romandronnikov@gmai.com>
2019-11-07 22:45:05 +0300

    add check primal array

--- array/concat.test.js
+++ array/concat.test.js
@@ -1,11 +1,15 @@
 import { expect } from '../test/utils.js'
-import { test } from './utils.test.js'
 
 export const testConcat = () => {
     const testResults = []
     expect(testResults, [1, 2, 3].concat([4, 5, 6]), [1, 2, 3, 4, 5, 6])
     expect(testResults, [].concat([]), [])
     expect(testResults, ['JS', 'реально', 'крутой', 'язык'].concat('но', ['придётся'], 'попыхтеть'), ['JS', 'реально', 'крутой', 'язык', 'но', 'придётся', 'попыхтеть'])
+
+    const primalArray1 = [1, 2, 3]
+    expect(testResults, primalArray1, [1, 2, 3])
+
+    
 
     return {
         name: 'testConcat',

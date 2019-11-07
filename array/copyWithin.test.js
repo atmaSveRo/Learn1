d62f1db1a9431fba9e7d45d6c894bce8b5c8a57c
atmaSveRo <romandronnikov@gmai.com>
2019-11-07 23:49:31 +0300

    add check primal array

--- array/copyWithin.test.js
+++ array/copyWithin.test.js
@@ -7,6 +7,11 @@ export const testCopyWithin = () => {
     expect(testResults, [55, 72, 105, 1228, 7, 78].copyWithin(4, 1, 4), [55, 72, 105, 1228, 72, 105])
     expect(testResults, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].copyWithin(5, 3, 6), [1, 2, 3, 4, 5, 4, 5, 6, 9, 10 ])
 
+    const primalArray = [1, 2, 3, 4, 5]
+    const newArray = primalArray.copyWithin(0, 3)
+    expect(testResults, primalArray, [1, 2, 3, 4, 5])
+    expect(testResults, primalArray, newArray)
+
     return {
         name: 'testCopyWithin',
         results: testResults

9214458f60058312fda8f64651bc6f9e3dd10795
atmaSveRo <romandronnikov@gmai.com>
2019-11-10 23:44:04 +0300

    add check primal array

--- array/sort.test.js
+++ array/sort.test.js
@@ -1,10 +1,15 @@
 import { expectEqual } from '../test/utils_equals.js'
+import { expectNotEqual } from '../test/utils_notEquals.js'
 
 export const testSort = () => {
     const testResults = []
     expectEqual(testResults, [2, 4, 1, 5, 3].sort((a, b) => a - b), [1, 2, 3, 4, 5])
     expectEqual(testResults, [1, 2, 11, 22, 111, 222].sort((a, b) => a - b), [1, 2, 11, 22, 111, 222])
     expectEqual(testResults, ['1', '2', '11', '22', '111', '222'].sort(), ['1', '11', '111', '2', '22', '222'])
+
+    const primalArray = [2, 4, 1, 5, 3]
+    const newArray = [2, 4, 1, 5, 3].sort((a, b) => a - b)
+    expectNotEqual(testResults, primalArray, newArray)
 
     return {
         name: 'testSort',
